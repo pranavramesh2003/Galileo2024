c3283cee1f5c341e6a2c153738dac6b5e6de1df9
pranavramesh2003 <cs22b015@smail.iitm.ac.in>
2024-03-02 12:56:38 +0530

    initial-commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nvidia/galileo2024/src
+# Build directory: /home/nvidia/galileo2024/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("traversal2")
---
+++ build/traversal/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nvidia/galileo2024/src/traversal
+# Build directory: /home/nvidia/galileo2024/build/traversal
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
